52ab51f76f263ba3057e422fa59302d7a764ea4c
Mona Tagelsir Sharif <91035196+monatagelsir7@users.noreply.github.com>
2024-10-06 20:59:15 +0000

    fix lint and extract

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,24 +1,21 @@
 """
 Extract a dataset from a URL like Kaggle or data.gov. 
-JSON or CSV formats tend to work well
+JSON or CSV formats tend to work well.
 
-marriage dataset
+Marriage dataset.
 """
 import requests
 
 def extract(
-    url="""
-    https://github.com/fivethirtyeight/
-    data/raw/refs/heads/master/births/
-    US_births_2000-2014_SSA.csv
-    """,
-    file_path="data/births.csv"):
-    """"Extract a url to a file path"""
+    url="https://github.com/fivethirtyeight/data/raw/refs/heads/master/births/"
+        "US_births_2000-2014_SSA.csv",
+    file_path="data/births.csv"
+):
+    """Extract a URL to a file path."""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
     return file_path
-
 
 
 
