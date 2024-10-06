8f51474720dac4c4dc5d3305712e952eb2c35022
Mona Tagelsir Sharif <91035196+monatagelsir7@users.noreply.github.com>
2024-10-06 21:22:27 +0000

    fix load

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 #load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/mts79-sqlite-lab/data/births.csv"):
+def load(dataset="data/births.csv"):
     """"Transforms and Loads data into the local SQLite3 database"""
 
     #prints the full working directory and path
