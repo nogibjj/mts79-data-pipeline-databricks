9f3de24d991239e97983ee8c0ebe03779b1b31b6
Mona Tagelsir Sharif <91035196+monatagelsir7@users.noreply.github.com>
2024-10-16 23:49:23 +0000

    extract two datasets

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,18 +5,27 @@ JSON or CSV formats tend to work well.
 Births dataset.
 """
 import os
+#import pandas as pd
 import requests
 
 def extract(
     url="https://github.com/fivethirtyeight/data/raw/refs/heads/master/births/"
         "US_births_2000-2014_SSA.csv",
-    file_path="data/births.csv"
+    url2 = "https://github.com/fivethirtyeight/data/raw/refs/heads/master/births/"
+        "US_births_1994-2003_CDC_NCHS.csv",
+    file_path="data/births2000.csv",
+    file_path2 ="data/births1994.csv",
 ):
     """Extract a URL to a file path."""
     # Create the directory if it doesn't exist
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    os.makedirs(os.path.dirname(file_path2), exist_ok=True)
     
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
-    return file_path
+    with requests.get(url2) as r:
+        with open(file_path2, 'wb') as f:
+            f.write(r.content)
+
+    return file_path, file_path2
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,47 +1,70 @@
 """
-Transforms and Loads data into the local SQLite3 database
+Transforms and Loads data into Azure Databricks
 """
-import sqlite3
-import csv
 import os
-
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="data/births.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
-
-    #prints the full working directory and path
-    print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    next(payload)
-    conn = sqlite3.connect('birthsDB.db')
-    c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS USBirths")
-    c.execute("""
-    CREATE TABLE USBirths (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        year INTEGER,
-        month INTEGER,
-        date_of_month INTEGER,
-        day_of_week INTEGER,
-        births INTEGER
-    )
-""")
-
-    #insert
-    c.executemany(
-        """
-        INSERT INTO USBirths (
-            year, 
-            month, 
-            date_of_month, 
-            day_of_week, 
-            births
-            ) 
-            VALUES (?, ?, ?, ?, ?)""",
-        payload,
-    )
+from databricks import sql
+import pandas as pd
+from dotenv import load_dotenv
 
 
-    conn.commit()
-    conn.close()
-    return "birthsDB.db"
+# load the csv file and insert into databricks
+def load(dataset="data/births2000.csv", dataset2="data/births1994"):
+    """Transforms and Loads data into the local databricks database"""
+    df = pd.read_csv(dataset, delimiter=",", skiprows=1)
+    df2 = pd.read_csv(dataset2, delimiter=",", skiprows=1)
+    load_dotenv()
+    server_h = os.getenv("SERVER_HOSTNAME")
+    access_token = os.getenv("PERSONAL_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        # INSERT TAKES TOO LONG
+        # c.execute("DROP TABLE IF EXISTS ServeTimesDB")
+        c.execute("SHOW TABLES FROM default LIKE 'serve*'")
+        result = c.fetchall()
+        # takes too long so not dropping anymore
+        # c.execute("DROP TABLE IF EXISTS EventTimesDB")
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS ServeTimesDB (
+                    id int,
+                    server string,
+                    seconds_before_next_point int,
+                    day string,
+                    opponent string,
+                    game_score string,
+                    sets int,
+                    game string
+                )
+            """
+            )
+            # insert
+            for _, row in df.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO ServeTimesDB VALUES {convert}")
+        c.execute("SHOW TABLES FROM default LIKE 'event*'")
+        result = c.fetchall()
+        # c.execute("DROP TABLE IF EXISTS EventTimesDB")
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS EventTimesDB (
+                    id int,
+                    tournament string,
+                    surface string,
+                    seconds_added_per_point string,
+                    years string
+                )
+                """
+            )
+            for _, row in df2.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO EventTimesDB VALUES {convert}")
+        c.close()
+
+    return "success"
